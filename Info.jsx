ad479e510d6949dbed3a4e6df0bbe6c37554def6
ham-h <64425236+ham-h@users.noreply.github.com>
2021-06-26 17:17:00 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>Web Development with React.js</h1>
+      <p>A basic web development React Js Bootcamp</p>
+    </div>
+  );
+}
+export default Info;
